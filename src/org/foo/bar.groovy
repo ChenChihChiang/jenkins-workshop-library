ceb347c6641dee01cc1ab29a53a3ac16da614aef
Chih Chiang Chen <john.c.chen@delta.com.tw>
2020-06-03 14:32:58 +0800

    Update bar.groovy

--- src/org/foo/bar.groovy
+++ src/org/foo/bar.groovy
@@ -2,18 +2,18 @@ package org.foo
 
     class utils implements Serializable {
 
-        def host1 = "Rico";
-        def host2 = "John";
-        def topic = "jenkins-workshop";
-        def organizer = "devops";
-        def date = "2020-06-21";
+        def host1 = "Rico"
+        def host2 = "John"
+        def topic = "jenkins-workshop"
+        def organizer = "devops"
+        def date = "2020-06-21"
         
         def showhost() {
-            println "host1 = ${super.host1}";
+            return "host1 = ${super.host1}"
         }
         
         def showcommitid() {
-            getGitCommitId();
+            getGitCommitId()
         }
         
         def getVersion(String BUILD_NUMBER, String GIT_COMMIT) {
