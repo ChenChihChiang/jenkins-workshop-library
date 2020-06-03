9e5e7ad29879d817263224e06192346a0edc9be1
Chih Chiang Chen <john.c.chen@delta.com.tw>
2020-06-03 15:21:43 +0800

    Update utils1.groovy

--- src/org/foo/utils1.groovy
+++ src/org/foo/utils1.groovy
@@ -1,8 +1,32 @@
 package org.foo
 
 class utils1 implements Serializable {
-	
-	String getGitCommitId() {
-		return sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
-	}
+
+  private final def script
+
+  utils1(def script) {
+    this.script = script
+  }
+
+  String commitMessage() {
+    trimOutput("git log --format=%B -n 1 HEAD | head -n 1", 180)
+  }
+
+  String commitAuthor() {
+    trimOutput("git log --format=\'%an\' -n 1 HEAD", 80)
+  }
+
+  String commitHash() {
+    trimOutput("git rev-parse HEAD", 7)
+  }
+
+  private String trimOutput(String script, int maxLength) {
+    String content = this.script.sh(script: script, returnStdout: true)
+    content.substring(0, Math.min(maxLength, content.length())).trim()
+  }
+
+  boolean isMasterBranch() {
+    script.env.BRANCH_NAME == 'master'
+  }
+
 }
