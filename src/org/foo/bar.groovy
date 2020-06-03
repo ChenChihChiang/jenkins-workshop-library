00aa7eba8a074ebe561e873481d1770ff3fd9caa
Chih Chiang Chen <john.c.chen@delta.com.tw>
2020-06-03 14:21:59 +0800

    Update bar.groovy

--- src/org/foo/bar.groovy
+++ src/org/foo/bar.groovy
@@ -13,10 +13,10 @@ package org.foo
         }
         
         def showcommitid() {
-  			getGitCommitId();
+            getGitCommitId();
         }
         
         def getVersion(String BUILD_NUMBER, String GIT_COMMIT) {
-    		return "Build_Number: ${BUILD_NUMBER}, Git_Commit: ${GIT_COMMIT}"
+            return "Build_Number: ${BUILD_NUMBER}, Git_Commit: ${GIT_COMMIT}"
     	}
     }
