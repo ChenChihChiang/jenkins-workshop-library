e638d9755f08373254b3a6ebfe418a37906d06d9
Chih Chiang Chen <john.c.chen@delta.com.tw>
2020-06-03 14:09:41 +0800

    Update bar.groovy

--- src/org/foo/bar.groovy
+++ src/org/foo/bar.groovy
@@ -7,16 +7,17 @@ package org.foo
   		topic = "jenkins-workshop";
   		organizer = "devops";
   		date = "2020-06-21";
+      
 
   		def showcommitid() {
-
+            println "host1 = ${host1}";
+        }
+        
+  		def showcommitid() {
   			getGitCommitId();
-
-  		}
+        }
         
     	def getVersion(String BUILD_NUMBER, String GIT_COMMIT) {
-
     		return "Build_Number: ${BUILD_NUMBER}, Git_Commit: ${GIT_COMMIT}"
-
     	}
     }
