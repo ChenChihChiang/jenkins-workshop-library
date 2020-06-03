fc06115c1a69eb8be6576aa40ade6c0c51c169c0
Chih Chiang Chen <john.c.chen@delta.com.tw>
2020-06-03 14:08:09 +0800

    Create bar.groovy

---
+++ src/org/foo/bar.groovy
@@ -0,0 +1,21 @@
+package org.foo
+
+    class utils implements Serializable {
+
+        host1 = "Rico";
+  		host2 = "John";
+  		topic = "jenkins-workshop";
+  		organizer = "devops";
+  		date = "2020-06-21";
+
+  		def showcommitid() {
+
+  			getGitCommitId();
+
+  		}
+    	def getVersion(String BUILD_NUMBER, String GIT_COMMIT) {
+
+    		return "Build_Number: ${BUILD_NUMBER}, Git_Commit: ${GIT_COMMIT}"
+
+    	}
+    }
